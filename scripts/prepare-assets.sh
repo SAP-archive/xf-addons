eefa07a505dc43cb43ccf43463f4834ade4a2550
Dobbs, Brendan <brendan.dobbs@sap.com>
2019-08-16 19:01:18 +0200

    Refactor openconnectors addon into 2 separate bundles. CRDs need to be configured first before we can create ConnectorInstances.

--- scripts/prepare-assets.sh
+++ scripts/prepare-assets.sh
@@ -47,7 +47,7 @@ function prepareAssets() {
 	    tar cfvz ${destination}/litmos-connector-0.1.0.tgz -C ./bundles/litmos-connector-0.1.0 .
         tar cfvz ${destination}/qualtrics-connector-0.1.0.tgz -C ./bundles/qualtrics-connector-0.1.0 .
         tar cfvz ${destination}/openconnectors-connector-0.1.0.tgz -C ./bundles/openconnectors-connector-0.1.0 .
-
+        tar cfvz ${destination}/openconnectors-account-0.1.0.tgz -C ./bundles/openconnectors-account-0.1.0 .
         echo "Processing done"
 }
 
