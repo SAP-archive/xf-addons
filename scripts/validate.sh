a1d78730feac43ef45e8cdb74cf998050a3053da
Andreas Thaler <andreas.thaler01@sap.com>
2020-05-06 22:08:30 +0200

    removal of connectors (#89)

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -8,12 +8,9 @@ readonly NC='\033[0m' # No Color
 
 function checkAssets() {
         echo "Checking addons"
-        helm lint ./addons/marketing-connector-0.1.0/chart/marketing-connector/ --values ./addons/marketing-connector-0.1.0/plans/default/values.yaml
         helm lint ./addons/marketing-mock-0.1.0/chart/marketing-mock/ --values ./addons/marketing-mock-0.1.0/plans/default/values.yaml
         helm lint ./addons/commerce-mock-0.1.0/chart/commerce-mock/ --values ./addons/commerce-mock-0.1.0/plans/default/values.yaml
         helm lint ./addons/c4c-mock-0.1.0/chart/c4c-mock/ --values ./addons/c4c-mock-0.1.0/plans/default/values.yaml
-	helm lint ./addons/litmos-connector-0.1.0/chart/litmos-connector/ --values ./addons/litmos-connector-0.1.0/plans/default/values.yaml
-        helm lint ./addons/openconnectors-connector-0.1.0/chart/openconnectors-connector/ --values ./addons/openconnectors-connector-0.1.0/plans/default/values.yaml
        
         echo "Processing done"
 }
